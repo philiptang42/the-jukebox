3786c78622f7585f6274d3c3722f4675da508896
philiptang42 <philiptang42@gmail.com>
2016-01-01 21:29:17 -0500

    part 1

--- the_jukebox.rb
+++ the_jukebox.rb
@@ -1,8 +1,27 @@
+require 'pry'
+
 class Playlist
   attr_accessor :songlist
 
-  def initialize(songlist)
-    @songlist = songlist
+  def initialize(songs)
+    @songs = [
+      ["Hello - Lionel Ritchie"],
+      ["Kokomo – The Beach Boys"],
+      ["Girl You Know It’s True – Milli Vanilli"],
+      ["Agadoo – Black Lace"],
+      ["Down Under - Men at Work"],
+      ["Nothing's Gonna Stop Us Now - Starship"],
+      ["Get Outta My Dreams, Get Into My Car - Billy Ocean"],
+      ["I Just Called To Say I Love You - Stevie Wonder"],
+      ["Hangin' Tough - New Kids on the Block"],
+      ["We Built This City - Starship"],
+      ["Wake Me Up Before You Go Go - Wham!"],
+      ["We Didn't Start The Fire - Billy Joel"],
+      ["I Wanna Dance With Somebody - Whitney Houston"],
+      ["U Can't Touch This - MC Hammer"]
+    ]
   end
 
 end
+
+binding.pry
