2ee879715f73d96584beffb71879c4ff591fdd99
philiptang42 <philiptang42@gmail.com>
2016-01-02 23:04:29 -0500

    part 4 progress

--- the_jukebox.rb
+++ the_jukebox.rb
@@ -33,6 +33,30 @@ class Playlist
   end
 
   def add_track(track)
+    song_list = [
+      ["Hello - Lionel Ritchie"],
+      ["Kokomo – The Beach Boys"],
+      ["Girl You Know It’s True – Milli Vanilli"],
+      ["Agadoo – Black Lace"],
+      ["Down Under - Men at Work"],
+      ["Nothing's Gonna Stop Us Now - Starship"],
+      ["Get Outta My Dreams, Get Into My Car - Billy Ocean"],
+      ["I Just Called To Say I Love You - Stevie Wonder"],
+      ["Hangin' Tough - New Kids on the Block"],
+      ["We Built This City - Starship"],
+      ["Wake Me Up Before You Go Go - Wham!"],
+      ["We Didn't Start The Fire - Billy Joel"],
+      ["I Wanna Dance With Somebody - Whitney Houston"],
+      ["U Can't Touch This - MC Hammer"]
+    ]
+    song = []
+    song << track
+    if song_list.include?(song)
+      song_list << song
+      @songs = song_list
+    else
+      return false
+    end
   end
 
 end
